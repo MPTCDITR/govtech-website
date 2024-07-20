cfd27034c49c7b6901e3e741adfa6c7ea5ba8453
Hikaru <hikaru.uchita@gmail.com>
2024-07-20 18:34:37 +0700

    fix: import env to next config

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,6 @@
+import { env } from "@/env";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
-import { env } from "./../../env";
 
 const client = createClient({
   url: env.DATABASE_URL,
