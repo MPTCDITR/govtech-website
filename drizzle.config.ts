e380b427dca56d2c07291981881f4dd699a6f18d
Hikaru <hikaru.uchita@gmail.com>
2024-07-20 16:54:48 +0700

    fix: apply biome fex

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { config } from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env' });
+config({ path: ".env" });
 
 export default defineConfig({
-  schema: './lib/db/schema.ts',
-  out: './migrations',
-  dialect: 'sqlite',
-  driver: 'turso',
+  schema: "./lib/db/schema.ts",
+  out: "./migrations",
+  dialect: "sqlite",
+  driver: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
     authToken: process.env.DATABASE_TOKEN!,
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,8 +1,8 @@
-import { config } from 'dotenv';
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import { createClient } from "@libsql/client";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/libsql";
 
-config({ path: '.env' });
+config({ path: ".env" });
 
 const client = createClient({
   url: process.env.DATABASE_URL!,
