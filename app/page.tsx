2d8bdd11e89eea9e89199ca5400cc2bb2b270f0c
PonlokTTR <ponlokpov2@gmail.com>
2024-07-24 11:19:08 +0700

    feat: merge with main

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import HeroSectionHeader from "@/components/homepage/hero-section-header"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Wellcome to GovTech Website 2024</h1>
-      </div>
+    <main className="flex min-h-screen flex-col justify-between ">
+      <HeroSectionHeader/>
     </main>
   );
 }
