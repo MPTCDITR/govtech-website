f3ca1ccd4c438f4e3c498d34fc37d56094d9d525
PonlokTTR <ponlokpov2@gmail.com>
2024-07-24 15:09:25 +0700

    refactor: linting

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { SupportedBySection } from "@/components/supported-by/supported-by-secti
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between ">
-      <SupportedBySection/>
+      <SupportedBySection />
     </main>
   );
 }
--- components/supported-by/supported-by-section.tsx
+++ components/supported-by/supported-by-section.tsx
@@ -3,19 +3,31 @@ import Text from "../ui/text";
 export const SupportedBySection = () => {
   return (
     <div className="columns-1 flex-col bg-[#DEDEDE]">
-    <div className="mt-5">
+      <div className="mt-5">
         <Text variant="heading" className="text-center text-[#0F45A8]">
-          SUPPORTED BY 
+          SUPPORTED BY
         </Text>
       </div>
       <div className="mx-4 my-20 grid grid-cols-2 justify-items-center md:grid-cols-6">
-          <img src="photo_2024-07-04_11-35-40-removebg-preview 1.png" alt="" className="h-full max-h-36"/>
-          <img src="dgc 1.png" alt="" className="h-full max-h-36 "/>
-          <img src="photo_2024-07-04_11-35-40-removebg-preview 1.png" alt="" className="h-full max-h-36"/>
-          <img src="dgc 1.png" alt="" className="h-full max-h-36 "/>
-          <img src="photo_2024-07-04_11-35-40-removebg-preview 1.png" alt="" className="h-full max-h-36"/>
-          <img src="dgc 1.png" alt="" className="h-full max-h-36"/>
+        <img
+          src="photo_2024-07-04_11-35-40-removebg-preview 1.png"
+          alt=""
+          className="h-full max-h-36"
+        />
+        <img src="dgc 1.png" alt="" className="h-full max-h-36 " />
+        <img
+          src="photo_2024-07-04_11-35-40-removebg-preview 1.png"
+          alt=""
+          className="h-full max-h-36"
+        />
+        <img src="dgc 1.png" alt="" className="h-full max-h-36 " />
+        <img
+          src="photo_2024-07-04_11-35-40-removebg-preview 1.png"
+          alt=""
+          className="h-full max-h-36"
+        />
+        <img src="dgc 1.png" alt="" className="h-full max-h-36" />
       </div>
-</div>
-  )
-}
+    </div>
+  );
+};
