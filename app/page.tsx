5834cc279ebfccada54cf6dc38cd13f72c97382c
2001st <xx1019jun96@gmail.com>
2024-07-25 15:56:46 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import SpeakerSection from "@/components/speaker-section/speaker-section.tsx";
-
+import HeroSectionHeader from "@/components/homepage/hero-section-header";
 
 import SpeakerPage from "@/app/speaker-page.tsx";
 
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
+        <HeroSectionHeader />
         <h1>Wellcome to GovTech Website 2024</h1>
         <SpeakerSection />
         
