538324942ddd3be55b6f910472cbb7eb67023f17
PonlokTTR <ponlokpov2@gmail.com>
2024-07-25 13:33:12 +0700

    feat: change name(subheading, body) on about-govtech page

--- components/about-govtech/about-govtech.tsx
+++ components/about-govtech/about-govtech.tsx
@@ -5,18 +5,18 @@ export default function AboutGovtech() {
   return (
     <div className=" mx-auto mt-5 p-5 md:flex">
       <div className="max-w-md ">
-        <Text variant="heading" className="text-center text-[#0F45A8]">
+        <Text variant="subheading" className="text-center text-[#0F45A8]">
           About Govtech
         </Text>
-        <Text variant="smallheading" className="mt-12 ">
+        <Text variant="body" className="mt-12 ">
           It was popularised in the 1960s with the release of Letraset sheets containing Lorem
           Ipsum passages, and more recently with desktop publishing software like Aldus
           PageMaker including versions of Lorem Ipsum.
         </Text>
-        <Text variant="smallheading" className="mt-12">
+        <Text variant="body" className="mt-12">
           March 14, 2024
         </Text>
-        <Text variant="smallheading" className="mt-5">
+        <Text variant="body" className="mt-5">
           It is a long established fact that a reader will be distracted by the readable content
           of a page when looking at its layout
         </Text>
