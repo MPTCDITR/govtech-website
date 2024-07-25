5712af1605f58c6594c297826638f862034538ae
2001st <xx1019jun96@gmail.com>
2024-07-25 16:54:46 +0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import HeroSectionHeader from "@/components/homepage/hero-section-header";
+import SpeakerSection from "@/components/speaker-section/speaker-section";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-between ">
+      <HeroSectionHeader />
+      <SpeakerSection />
+    </main>
+  );
+}
