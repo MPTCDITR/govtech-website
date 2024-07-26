ad05224c412c60980cc58a782771c1dd5665b84c
PonlokTTR <ponlokpov2@gmail.com>
2024-07-26 11:30:22 +0700

    refactor: linting

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Agenda from "@/components/agenda/agenda";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between">
-      <Agenda/>
+      <Agenda />
     </main>
   );
 }
