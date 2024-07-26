1eb859d0c0ebd45269620337f2e1619cacfa9c64
2001st <xx1019jun96@gmail.com>
2024-07-26 14:55:32 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col justify-between ">
       <HeroSectionHeader />
       <SpeakerSection />
-    
     </main>
   );
 }
