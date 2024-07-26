ebcebfee990afaac9a76e10f87800ae3ced3fb62
PonlokTTR <ponlokpov2@gmail.com>
2024-07-26 15:54:59 +0700

    feat: conflicting files

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { SupportedBySection } from "@/components/supported-by/supported-by-secti
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between ">
-      <HeroSectionHeader/>
+      <HeroSectionHeader />
       <SupportedBySection />
     </main>
   );
