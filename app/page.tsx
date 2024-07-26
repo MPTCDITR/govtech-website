00c070f5bc6d6ee2408bcb591ff671af8d779c53
PonlokTTR <ponlokpov2@gmail.com>
2024-07-26 15:56:53 +0700

    feat: conflicting files

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import HeroSectionHeader from "@/components/homepage/hero-section-header";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between ">
-      <HeroSectionHeader/>
+      <HeroSectionHeader />
       <Agenda />
     </main>
   );
