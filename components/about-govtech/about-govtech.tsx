203d61088522c50325956f9dd31373cc25d587af
PonlokTTR <ponlokpov2@gmail.com>
2024-07-29 10:43:06 +0700

    feat: resolve comment

--- components/about-govtech/about-govtech.tsx
+++ components/about-govtech/about-govtech.tsx
@@ -3,27 +3,27 @@ import Text from "../ui/text";
 
 export default function AboutGovtech() {
   return (
-    <div className=" mx-auto mt-5 p-5 md:flex">
-      <div className="max-w-md">
-        <Text variant="subheading" className="text-[#0F45A8]">
+    <div className="mx-auto flex flex-col p-4 lg:flex-row lg:justify-around">
+      <div className="w-[360px]">
+        <Text variant="subheading" className="text-primary">
           About Govtech
         </Text>
-        <Text variant="body" className="mt-12 max-w-96">
+        <Text variant="body" className="mt-8">
           It was popularised in the 1960s with the release of Letraset sheets containing Lorem
           Ipsum passages, and more recently with desktop publishing software like Aldus
           PageMaker including versions of Lorem Ipsum.
         </Text>
-        <Text variant="body" className="mt-12">
+        <Text variant="body" className="mt-14">
           March 14, 2024
         </Text>
-        <Text variant="body" className="mt-5 ">
+        <Text variant="body" className="mt-8">
           It is a long established fact that a reader will be distracted by the readable content
           of a page when looking at its layout
         </Text>
-        <Button className="mt-8 ml-28 bg-primary text-white">View Agenda</Button>
+        <Button className="bg-primary text-white">View Agenda</Button>
       </div>
       <div className="mt-8 md:mt-0">
-        <img className="h-96 h-auto w-full max-w-3xl" src="HRTechx 1.png" alt="" />
+        <img className="w-full max-w-2xl" src="HRTechx 1.png" alt="" />
       </div>
     </div>
   );
