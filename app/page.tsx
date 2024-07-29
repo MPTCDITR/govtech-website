94e7506a73092ad394b6fc211cf57749bb15703e
Vichheka <vichhekaouk30@gmail.com>
2024-07-29 11:40:15 +0700

    Implement Goal Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { GoalSection } from "@/components/goal-section";
+
 import HeroSectionHeader from "@/components/homepage/hero-section-header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between ">
       <HeroSectionHeader />
+      <GoalSection />
     </main>
   );
 }
---
+++ components/goal-section/index.tsx
@@ -0,0 +1,18 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "../../components/ui/card";
+
+export const GoalSection = () => {
+  return (
+    <Card className="p-3 m-auto">
+      <CardHeader className="h-auto w-96 p-4">
+        <CardTitle className="mt-2 mb-3 font-bold text-gray-900 text-lg tracking-tight dark:text-white">
+          Goal Card
+        </CardTitle>
+        <CardDescription className="line-clamp-5 font-normal text-gray-700 dark:text-gray-400">
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem
+          Ipsum passages, and more recently with desktop publishing software like Aldus
+          PageMaker including versions of Lorem Ipsum.
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  );
+};
