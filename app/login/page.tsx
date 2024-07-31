51376d5551a36c2094f178db6c0e5f7ac089f35b
Sreynoch <sothunsreynoch@gmail.com>
2024-07-31 14:18:31 +0700

    update responsive

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,20 +4,20 @@ import { Label } from "@/components/ui/label";
 
 export default function Login() {
   return (
-    <div className="grid lg:grid-cols-2 md:grid-cols-1">
+    <div className="grid md:grid-cols-1 lg:grid-cols-2">
       <div className="flex items-center">
         <form className="w-full justify-between px-4 lg:px-20">
           <div className="w-full space-y-6">
             <div className="grid justify-items-center px-20">
               <Label>Logo</Label>
             </div>
-            <div className="flex flex-col space-y-1.5">
+            <div className="space-y-1.5">
               <Label htmlFor="name">Username / Phone Number</Label>
               <Input id="frist name" placeholder="Frist Name" />
             </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Password</Label>
-              <Input id="last name" placeholder="Last Name" />
+            <div className="space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" placeholder="Password" />
             </div>
             <div className="flex justify-between">
               <Button className="text-white" type="submit">
