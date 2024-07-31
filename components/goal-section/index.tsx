3592a693916c32b5116c4e792501889cf1613b13
Vichheka <vichhekaouk30@gmail.com>
2024-07-31 23:04:54 +0700

    Giving Phone viewport size to the GoalCard

--- components/goal-section/index.tsx
+++ components/goal-section/index.tsx
@@ -2,7 +2,7 @@ import { Card, CardDescription, CardHeader, CardTitle } from "../../components/u
 
 export const GoalSection = () => {
   return (
-    <Card className="p-3 m-auto h-56 w-96">
+    <Card className="m-auto h-auto w-96 border-red-600 p-3 max-sm:max-w-96 ">
       <CardHeader className="p-4">
         <CardTitle className="mt-2 mb-3 font-bold text-gray-900 text-lg tracking-tight dark:text-white">
           Goal Card
