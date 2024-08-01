d43a79bbadb0234ceb029e45698c203d666bca38
Ave Aristov <chantepkik@gmail.com>
2024-08-01 15:52:14 +0700

    chore: reformat file

--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -1,34 +1,34 @@
-import SpeakerCard from "@/components/speaker-card/speaker-card";
+import SpeakerCard from '@/components/speaker-card/speaker-card';
 
 export default function SpeakerPage() {
-  return (
-    <main className="mt-8 mb-20 flex justify-center">
-      <div className="w-full max-w-7xl px-4">
-        <div className="text-center">
-          <h1 className="mb-4 font-bold text-4xl">Speaker Section</h1>
-          <p className="mb-4 text-lg">List of Speakers</p>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-0 gap-y-9">
-            <div className="flex items-center justify-center">
-              <SpeakerCard />
+    return (
+        <main className='mt-8 mb-20 flex justify-center'>
+            <div className='w-full max-w-7xl px-4'>
+                <div className='text-center'>
+                    <h1 className='mb-4 font-bold text-4xl'>Speaker Section</h1>
+                    <p className='mb-4 text-lg'>List of Speakers</p>
+                    <div className='grid grid-cols-1 gap-x-0 gap-y-9 md:grid-cols-2 lg:grid-cols-3'>
+                        <div className='flex items-center justify-center'>
+                            <SpeakerCard />
+                        </div>
+                        <div className='flex items-center justify-center'>
+                            <SpeakerCard />
+                        </div>
+                        <div className='flex items-center justify-center'>
+                            <SpeakerCard />
+                        </div>
+                        <div className='flex items-center justify-center'>
+                            <SpeakerCard />
+                        </div>
+                        <div className='flex items-center justify-center'>
+                            <SpeakerCard />
+                        </div>
+                        <div className='flex items-center justify-center'>
+                            <SpeakerCard />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="flex items-center justify-center">
-              <SpeakerCard />
-            </div>
-            <div className="flex items-center justify-center">
-              <SpeakerCard />
-            </div>
-            <div className="flex items-center justify-center">
-              <SpeakerCard />
-            </div>
-            <div className="flex items-center justify-center">
-              <SpeakerCard />
-            </div>
-            <div className="flex items-center justify-center">
-              <SpeakerCard />
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
--- components/supported-by/supported-by-section.tsx
+++ components/supported-by/supported-by-section.tsx
@@ -1,15 +1,15 @@
-import ImageGrid from "@/components/supported-by/imagegrid";
-import Text from "../ui/text";
+import ImageGrid from '@/components/supported-by/imagegrid';
+import Text from '../ui/text';
 
 export const SupportedBySection = () => {
-  return (
-    <div className="flex flex-col bg-gray-300">
-      <div className="mt-5">
-        <Text variant="subheading" className="text-center text-primary">
-          SUPPORTED BY
-        </Text>
-      </div>
-      <ImageGrid />
-    </div>
-  );
+    return (
+        <div className='flex flex-col bg-gray-300'>
+            <div className='mt-5'>
+                <Text variant='subheading' className='text-center text-primary'>
+                    SUPPORTED BY
+                </Text>
+            </div>
+            <ImageGrid />
+        </div>
+    );
 };
