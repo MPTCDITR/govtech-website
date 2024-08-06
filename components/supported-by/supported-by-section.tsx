8a7c2829b71d2c1f3f07302bb8170a4b625e63fa
Boti <s.visethboti.gmail.com>
2024-08-06 14:05:27 +0700

    feat: linting

--- components/supported-by/supported-by-section.tsx
+++ components/supported-by/supported-by-section.tsx
@@ -9,7 +9,7 @@ const images = [
 
 export const SupportedBySection = () => {
     return (
-        <div className='grid·list-none·grid-cols-2·place-items-center·gap-16·p-8·md:grid-cols-4·lg:grid-cols-6'>
+        <div className='grid list-none grid-cols-2 place-items-center gap-16 p-8 md:grid-cols-4 lg:grid-cols-6'>
             {images.map((image, index) => (
                 <img key={index} src={image.src} alt={image.alt} className='h-full max-h-36' />
             ))}
