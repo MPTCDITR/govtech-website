8fe30866d142e020c2ed11ae5f07c29597fab967
Boti <s.visethboti.gmail.com>
2024-08-06 15:39:05 +0700

    feat: improve about govtech

--- components/about-govtech/about-govtech.tsx
+++ components/about-govtech/about-govtech.tsx
@@ -1,29 +1,35 @@
+import TextElement from '../TextElement';
 import { Button } from '../ui/button';
 import Text from '../ui/text';
 
 export default function AboutGovtech() {
     return (
-        <div className='mx-auto flex flex-col p-4 lg:flex-row lg:justify-around'>
-            <div className='w-[360px]'>
-                <Text variant='subheading' className='text-primary'>
+        <div className='container flex flex-col items-center gap-8 p-4 lg:flex-row lg:items-start lg:justify-around'>
+            <div className='grid w-full gap-6'>
+                <TextElement variant='subheading' className='text-primary'>
                     About Govtech
-                </Text>
-                <Text variant='body' className='mt-8'>
-                    It was popularised in the 1960s with the release of Letraset sheets
-                    containing Lorem Ipsum passages, and more recently with desktop publishing
-                    software like Aldus PageMaker including versions of Lorem Ipsum.
-                </Text>
-                <Text variant='body' className='mt-14'>
-                    March 14, 2024
-                </Text>
-                <Text variant='body' className='mt-8'>
-                    It is a long established fact that a reader will be distracted by the
-                    readable content of a page when looking at its layout
-                </Text>
-                <Button className='bg-primary text-white'>View Agenda</Button>
+                </TextElement>
+                <TextElement variant='body'>
+                    The GovTech Conference is a gathering where Cambodian ministries,
+                    businesses, and development partners come together to collaboratively shape
+                    the future of digital government technology. This event provides a platform
+                    to explore how cutting-edge technology is revolutionizing public services,
+                    fostering trust within the community, and strengthening citizen engagement.
+                </TextElement>
+                <TextElement variant='body'>
+                    The Conference take place on September 17, 2024
+                </TextElement>
+                <TextElement variant='body'>
+                    Attendees can expect discussions, demonstrations, and partnerships focused
+                    on creating a more transparent, efficient, and citizen-centric government.
+                    The conference highlights the collaborative efforts between the public and
+                    private sectors to advance the nation’s digital infrastructure and improve
+                    the quality of life for all Cambodians.
+                </TextElement>
+                <Button className='w-48 bg-primary text-white'>View Agenda</Button>
             </div>
-            <div className='mt-8 md:mt-0'>
-                <img className='w-full max-w-2xl' src='HRTechx 1.png' alt='' />
+            <div className='w-full lg:max-w-2xl'>
+                <img className='w-full' src='HRTechx 1.png' alt='' />
             </div>
         </div>
     );
