b000d6878ac162ab22376273e8cbfd14859cc699
PonlokTTR <ponlokpov2@gmail.com>
2024-08-06 13:10:01 +0700

    feat: merge with main

--- components/about-govtech/about-govtech.tsx
+++ components/about-govtech/about-govtech.tsx
@@ -5,20 +5,19 @@ export default function AboutGovtech() {
     return (
         <div className='mt-8 flex flex-col justify-center gap-x-32 px-1 lg:flex-row'>
             <div className='mt-5 grid w-[385px]'>
-                    <Text variant='subheading' className='text-center text-primary'>
-                        About Govtech
-                    </Text>
-                    <Text variant='body'>
-                        It was popularised in the 1960s with the release of Letraset sheets
-                        containing Lorem Ipsum passages, and more recently with desktop
-                        publishing software like Aldus PageMaker including versions of Lorem
-                        Ipsum.
-                    </Text>
-                    <Text variant='body'>March 14, 2024</Text>
-                    <Text variant='body'>
-                        It is a long established fact that a reader will be distracted by the
-                        readable content of a page when looking at its layout
-                    </Text>
+                <Text variant='subheading' className='text-center text-primary'>
+                    About Govtech
+                </Text>
+                <Text variant='body'>
+                    It was popularised in the 1960s with the release of Letraset sheets
+                    containing Lorem Ipsum passages, and more recently with desktop publishing
+                    software like Aldus PageMaker including versions of Lorem Ipsum.
+                </Text>
+                <Text variant='body'>March 14, 2024</Text>
+                <Text variant='body'>
+                    It is a long established fact that a reader will be distracted by the
+                    readable content of a page when looking at its layout
+                </Text>
                 <div className='grid justify-items-center'>
                     <div className='mt-5 flex w-[150px] justify-center'>
                         <Button className='bg-primary text-white hover:bg-sky-500'>
