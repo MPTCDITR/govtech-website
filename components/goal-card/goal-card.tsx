de892c0501c6ea4b640c83c600fbcbc04c8735b7
Boti <s.visethboti.gmail.com>
2024-08-06 12:54:46 +0700

    feat: add goalCard and goalSection

--- components/goal-card/goal-card.tsx
+++ components/goal-card/goal-card.tsx
@@ -1,17 +1,15 @@
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
-export default function GoalCard() {
-    return (
-        <Card>
-            <CardHeader className='w-96 h-[200px] p-4'>
-                <CardTitle className='mt-2 mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                    Name
-                </CardTitle>
-                <CardDescription className='font-normal text-gray-700 dark:text-gray-400'>
-                    The generated Lorem Ipsum is therefore always free from repetition, injected
-                    humour.
-                </CardDescription>
-            </CardHeader>
-        </Card>
-    );
+interface GoalCardProps {
+    title: string;
+    description: string;
 }
+
+const GoalCard = ({ title, description }: GoalCardProps) => (
+    <Card className='flex h-full flex-col items-start gap-2 rounded-lg border bg-card p-5 text-card-foreground'>
+        <CardTitle className='text-lg text-primary'>{title}</CardTitle>
+        <CardDescription className='line-clamp-4 text-pretty'>{description}</CardDescription>
+    </Card>
+);
+
+export default GoalCard;
