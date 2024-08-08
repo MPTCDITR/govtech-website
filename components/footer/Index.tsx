bc32b1ef2b126eb38e2df68eec4d3197dca30129
PonlokTTR <ponlokpov2@gmail.com>
2024-08-08 16:20:21 +0700

    feat: conflicting files

--- components/footer/Index.tsx
+++ components/footer/Index.tsx
@@ -8,11 +8,11 @@ export default function Footer() {
         <footer className=' bg-primary px-8 pt-5 pb-5 text-white'>
             <div className='flex grid list-none grid-cols-1 flex-col gap-8 p-4 md:grid-cols-3 lg:grid-cols-3'>
                 <div className='flex flex-col gap-4'>
-                <Link href='/'>
+                    <Link href='/'>
                         <Text variant='subheading'>GovTech</Text>
                     </Link>
                 </div>
-                <div >
+                <div>
                     <QuickLink />
                 </div>
                 <div>
@@ -21,7 +21,8 @@ export default function Footer() {
             </div>
             <div className='border ' />
             <Text variant='body' className='mt-5 text-center text-slate-300'>
-                2024 GovTech - Department of digital Government transformation - All Rights Reserved.
+                2024 GovTech - Department of digital Government transformation - All Rights
+                Reserved.
             </Text>
         </footer>
     );
