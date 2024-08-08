7cf03efde188526522aa35572eab02f3bfa5f09a
2001st <xx1019jun96@gmail.com>
2024-08-08 16:15:10 +0700

    Add files via upload

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,49 +1,19 @@
-import TextElement from '@/components/TextElement';
-import AboutGovtech from '@/components/about-govtech/about-govtech';
-import GoalSection from '@/components/goal-section/goal-section';
-import HeroSectionHeader from '@/components/homepage/hero-section-header';
-import SpeakerSection from '@/components/speaker-section/speaker-section';
-import { SupportedBySection } from '@/components/supported-by/supported-by-section';
+import AboutGovtech from "@/components/about-govtech/about-govtech";
+import GoalSection from "@/components/goal-section/goal-section";
+import HeroSectionHeader from "@/components/homepage/hero-section-header";
+import SpeakerSection from "@/components/speaker-section/speaker-section";
+import { SupportedBySection } from "@/components/supported-by/supported-by-section";
 
 export const revalidate = 0;
 
 export default function Home() {
-    return (
-        <main className='flex min-h-screen flex-col justify-between gap-16 pb-16'>
-            <HeroSectionHeader />
-            <section aria-labelledby='about-govtech'>
-                <AboutGovtech />
-            </section>
-            <section aria-labelledby='supported-by' className='bg-primary p-8'>
-                <TextElement
-                    variant='subheading'
-                    id='supported-by'
-                    className='mb-4 text-center text-white'
-                >
-                    Supported By
-                </TextElement>
-                <SupportedBySection />
-            </section>
-            <section aria-labelledby='our-speakers'>
-                <TextElement
-                    variant='subheading'
-                    id='our-speakers'
-                    className='mb-4 text-center text-primary'
-                >
-                    Our Speakers
-                </TextElement>
-                <SpeakerSection />
-            </section>
-            <section aria-labelledby='our-goals'>
-                <TextElement
-                    variant='subheading'
-                    id='our-goals'
-                    className='mb-4 text-center text-primary'
-                >
-                    Our Goals
-                </TextElement>
-                <GoalSection />
-            </section>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col justify-between ">
+      <HeroSectionHeader />
+      <AboutGovtech />
+      <SupportedBySection />
+      <SpeakerSection />
+      <GoalSection />
+    </main>
+  );
 }
