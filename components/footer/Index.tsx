9dbe78aeafcfe971125a6cdaa5d5bba40ec6482b
Vangnea <vangnea.meng@gmail.com>
2024-08-08 09:58:23 +0700

    feat: govtech logo clickable to home

--- components/footer/Index.tsx
+++ components/footer/Index.tsx
@@ -1,14 +1,16 @@
+import Link from 'next/link';
 import Text from '../ui/text';
 import { Contact } from './Contact';
 import { QuickLink } from './QuickLink';
-import Link from 'next/link';
 
 export default function Footer() {
     return (
         <footer className=' bg-primary px-10 pt-5 pb-5 text-white'>
             <div className='flex flex-col gap-8 md:justify-between lg:flex-row lg:flex-wrap lg:justify-around lg:gap-5'>
                 <div className='flex flex-col gap-4'>
-                    <Link href='/'><Text variant='subheading'>GovTech</Text></Link>
+                    <Link href='/'>
+                        <Text variant='subheading'>GovTech</Text>
+                    </Link>
                 </div>
                 <div>
                     <Contact />
