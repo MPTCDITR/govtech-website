beae7e97281418617a40f3d11650b5ea5d83310e
2001st <xx1019jun96@gmail.com>
2024-08-09 13:55:17 +0700

    Add files via upload

--- app/[locale]/speakers/page.tsx
+++ app/[locale]/speakers/page.tsx
@@ -1,31 +1,14 @@
-import SpeakerCard from '@/components/speaker-card/speaker-card';
+import SpeakerCardList from "@/components/speaker-card/speaker-card-list";
 
 export default function SpeakerPage() {
     return (
-        <main className='mt-8 mb-20 flex justify-center'>
-            <div className='w-full max-w-7xl px-4'>
-                <div className='text-center'>
-                    <h1 className='mb-4 font-bold text-4xl'>Speaker Section</h1>
-                    <p className='mb-4 text-lg'>List of Speakers</p>
-                    <div className='grid grid-cols-1 gap-x-0 gap-y-9 md:grid-cols-2 lg:grid-cols-3'>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
+        <main className="mt-8 mb-20 flex justify-center">
+            <div className="w-full max-w-7xl px-4">
+                <div className="text-center">
+                    <h1 className="mb-4 font-bold text-4xl">Speaker Section</h1>
+                    <p className="mb-4 text-lg">List of Speakers</p>
+                    <div>
+                        <SpeakerCardList />
                     </div>
                 </div>
             </div>
