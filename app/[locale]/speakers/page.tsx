3c41ad6aca5ae9732ff67fb75a4d3835e63152cb
2001st <xx1019jun96@gmail.com>
2024-08-09 14:10:07 +0700

    Update page.tsx

--- app/[locale]/speakers/page.tsx
+++ app/[locale]/speakers/page.tsx
@@ -1,12 +1,12 @@
-import SpeakerCardList from "@/components/speaker-card/speaker-card-list";
+import SpeakerCardList from '@/components/speaker-card/speaker-card-list';
 
 export default function SpeakerPage() {
     return (
-        <main className="mt-8 mb-20 flex justify-center">
-            <div className="w-full max-w-7xl px-4">
-                <div className="text-center">
-                    <h1 className="mb-4 font-bold text-4xl">Speaker Section</h1>
-                    <p className="mb-4 text-lg">List of Speakers</p>
+        <main className='mt-8 mb-20 flex justify-center'>
+            <div className='w-full max-w-7xl px-4'>
+                <div className='text-center'>
+                    <h1 className='mb-4 font-bold text-4xl'>Speaker Section</h1>
+                    <p className='mb-4 text-lg'>List of Speakers</p>
                     <div>
                         <SpeakerCardList />
                     </div>
