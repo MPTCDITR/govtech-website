cf44e1ea3c97871fda5feda2572aa2c49174cd94
2001st <xx1019jun96@gmail.com>
2024-08-09 14:03:09 +0700

    Update speaker-card.tsx

--- components/speaker-card/speaker-card.tsx
+++ components/speaker-card/speaker-card.tsx
@@ -1,9 +1,4 @@
-import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 interface SpeakerProps {
     name: string;
@@ -13,18 +8,18 @@ interface SpeakerProps {
 
 export default function SpeakerCard({ name, bio, imageUrl }: SpeakerProps) {
     return (
-        <Card className="border border-gray-200">
-            <CardHeader className="max-w-full p-4">
+        <Card className='border border-gray-200'>
+            <CardHeader className='max-w-full p-4'>
                 <img
-                    className="h-auto w-full rounded-lg mb-3"
+                    className='mb-3 h-auto w-full rounded-lg'
                     src={imageUrl}
                     alt={name}
-                    style={{ maxWidth: "100%" }}
+                    style={{ maxWidth: '100%' }}
                 />
-                <CardTitle className="mt-2 mb-4 font-bold text-gray-900 text-xl tracking-tight dark:text-white">
+                <CardTitle className='mt-2 mb-4 font-bold text-gray-900 text-xl tracking-tight dark:text-white'>
                     {name}
                 </CardTitle>
-                <CardDescription className="font-normal text-gray-700 dark:text-gray-400">
+                <CardDescription className='font-normal text-gray-700 dark:text-gray-400'>
                     {bio}
                 </CardDescription>
             </CardHeader>
