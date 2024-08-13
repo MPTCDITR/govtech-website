0a4ab82ab75e3af36d7f2bbc7797b339054be395
2001st <xx1019jun96@gmail.com>
2024-08-13 13:18:01 +0700

    Add files via upload

--- app/[locale]/speakers/page.tsx
+++ app/[locale]/speakers/page.tsx
@@ -1,31 +1,19 @@
-import SpeakerCard from '@/components/speaker-card/speaker-card';
+import SpeakerCardList from '@/components/speaker-card/speaker-card-list';
+import TextElement from '@/components/TextElement';
 
 export default function SpeakerPage() {
     return (
-        <main className='mt-8 mb-20 flex justify-center'>
+        <main className='mt-4 mb-20 flex justify-center'>
             <div className='w-full max-w-7xl px-4'>
                 <div className='text-center'>
-                    <h1 className='mb-4 font-bold text-4xl'>Speaker Section</h1>
-                    <p className='mb-4 text-lg'>List of Speakers</p>
-                    <div className='grid grid-cols-1 gap-x-0 gap-y-9 md:grid-cols-2 lg:grid-cols-3'>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
+                    <TextElement
+                        variant='heading'
+                        className='mt-4 mb-10 text-primary'
+                    >
+                        Our Speakers
+                    </TextElement>
+                    <div>
+                        <SpeakerCardList />
                     </div>
                 </div>
             </div>
