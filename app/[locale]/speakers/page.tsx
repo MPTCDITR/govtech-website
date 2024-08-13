9cb3f4c9262635a8e4e4ec6be1618af8195067dd
2001st <xx1019jun96@gmail.com>
2024-08-13 13:26:37 +0700

    Update page.tsx

--- app/[locale]/speakers/page.tsx
+++ app/[locale]/speakers/page.tsx
@@ -1,15 +1,12 @@
-import SpeakerCardList from '@/components/speaker-card/speaker-card-list';
 import TextElement from '@/components/TextElement';
+import SpeakerCardList from '@/components/speaker-card/speaker-card-list';
 
 export default function SpeakerPage() {
     return (
         <main className='mt-4 mb-20 flex justify-center'>
             <div className='w-full max-w-7xl px-4'>
                 <div className='text-center'>
-                    <TextElement
-                        variant='heading'
-                        className='mt-4 mb-10 text-primary'
-                    >
+                    <TextElement variant='heading' className='mt-4 mb-10 text-primary'>
                         Our Speakers
                     </TextElement>
                     <div>
