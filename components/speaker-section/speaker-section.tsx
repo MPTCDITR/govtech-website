36c355ec9503b526c7fcf794e36d717114bf1567
2001st <xx1019jun96@gmail.com>
2024-08-13 13:17:22 +0700

    Add files via upload

--- components/speaker-section/speaker-section.tsx
+++ components/speaker-section/speaker-section.tsx
@@ -1,33 +1,15 @@
-import SpeakerCard from '@/components/speaker-card/speaker-card';
+import SpeakerCardList from '@/components/speaker-card/speaker-card-list';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 export default function SpeakerSection() {
     return (
-        <div className='flex justify-center'>
+        <div className='mt-8 flex justify-center'>
             <div className='w-full max-w-7xl px-4'>
-                <div className='text-center'>
-                    <div className='grid grid-cols-1 gap-x-0 gap-y-9 md:grid-cols-2 lg:grid-cols-3'>
-                        <div className='speaker-card-container flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='speaker-card-container flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='speaker-card-container flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='speaker-card-container flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='speaker-card-container flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                        <div className='speaker-card-container flex items-center justify-center'>
-                            <SpeakerCard />
-                        </div>
-                    </div>
-                    <div className='mt-8'>
+                <div className='text-center mb-15'>
+                    <div>
+                        <SpeakerCardList />
+
                         <Link href='/speakers'>
                             <Button className='bg-primary text-white hover:bg-sky-500'>
                                 <b>View all Speakers</b>
