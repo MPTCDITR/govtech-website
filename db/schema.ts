46124aca9472fc801df7dc9d66ecce0551cf7a37
Raksmey <rsmey8373@gmail.com>
2024-08-14 09:46:18 +0700

    chore: add todo

--- db/schema.ts
+++ db/schema.ts
@@ -28,12 +28,12 @@ export const userUpdateSchema = z.object({
     firstName: z.string().trim().min(1, { message: 'First Name required' }).max(100),
     lastName: z.string().trim().min(1, { message: 'Last Name required' }),
     gender: z.enum(['male', 'female', 'other'], { message: 'Select a gender' }),
-    //TO DO:  probably want a specific format phone number
+    //TO DO:  probably want a specific format phone numbers
     phoneNumber: z
         .string()
         .trim()
         .regex(/^(\+\d{1,3})?[-\s]?\(?(\d{1,4})\)?[-\s]?(\d{1,9})$/, {
-            message: '(+855) 12 345 678',
+            message: '(+855) 12 345 6789',
         }),
     occupation: z.string().trim().min(1).max(100, { message: 'Occupation required' }),
     workPlace: z.string().trim().min(1).max(100, { message: 'Work Place required' }),
