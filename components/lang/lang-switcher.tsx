647234338cbdf21094e5c0106772af269c350c6f
Vangnea <vangnea.meng@gmail.com>
2024-08-15 10:46:37 +0700

    feat: improve ui translation

--- components/lang/lang-switcher.tsx
+++ components/lang/lang-switcher.tsx
@@ -1,54 +1,43 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import { type ChangeEvent, type ReactNode, useTransition } from 'react';
+import { type ReactNode, useTransition } from 'react';
 
-import clsx from 'clsx';
 import { usePathname, useRouter } from '../../navigation';
 
 type Props = {
     children: ReactNode;
-    defaultValue: string;
-    label: string;
+    toggleLocale: string;
 };
 
-export default function LocaleSwitcherSelect({ children, defaultValue, label }: Props) {
+export default function LocaleSwitcherSelect({ children, toggleLocale }: Props) {
     const router = useRouter();
     const [isPending, startTransition] = useTransition();
     const pathname = usePathname();
     const params = useParams();
 
-    function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
-        const nextLocale = event.target.value;
+    function onToggleLocale() {
         startTransition(() => {
             router.replace(
                 // @ts-expect-error -- TypeScript will validate that only known `params`
                 // are used in combination with a given `pathname`. Since the two will
                 // always match for the current route, we can skip runtime checks.
                 { pathname, params },
-                { locale: nextLocale },
+                { locale: toggleLocale },
             );
             router.refresh();
         });
     }
 
     return (
-        <label
-            className={clsx(
-                'relative text-primary',
-                isPending && 'transition-opacity [&:disabled]:opacity-30',
-            )}
+        <button
+            type='button'
+            className={`relative ${isPending ? 'opacity-30' : ''}`}
+            onClick={onToggleLocale}
+            disabled={isPending}
+            aria-label={`Switch to ${toggleLocale}`}
         >
-            <p className='sr-only'>{label}</p>
-            <select
-                className='inline-flex appearance-none bg-transparent py-3 pr-6 pl-2'
-                defaultValue={defaultValue}
-                disabled={isPending}
-                onChange={onSelectChange}
-            >
-                {children}
-            </select>
-            <span className='pointer-events-none absolute top-[8px] right-2'>⌄</span>
-        </label>
+            {children}
+        </button>
     );
 }
--- components/lang/locale-switcher.tsx
+++ components/lang/locale-switcher.tsx
@@ -1,18 +1,20 @@
-import { useLocale, useTranslations } from 'next-intl';
-import { locales } from '../../config';
+import { useLocale } from 'next-intl';
+import Image from 'next/image';
+import enIcon from '../../public/lang-icon/en.svg';
+import kmIcon from '../../public/lang-icon/km.svg';
+// import { locales } from '../../config';
 import LocaleSwitcherSelect from './lang-switcher';
 
 export default function LocaleSwitcher() {
-    const t = useTranslations('LocaleSwitcher');
+    // const t = useTranslations('LocaleSwitcher');
     const locale = useLocale();
+    const toggleLocale = locale === 'km' ? 'en' : 'km';
+
+    const flagSrc = locale === 'km' ? enIcon : kmIcon;
 
     return (
-        <LocaleSwitcherSelect defaultValue={locale} label={t('label')}>
-            {locales.map((cur) => (
-                <option key={cur} value={cur}>
-                    {t('locale', { locale: cur })}
-                </option>
-            ))}
+        <LocaleSwitcherSelect toggleLocale={toggleLocale}>
+            <Image src={flagSrc} alt={`switch to ${toggleLocale}`} width={100} height={100} />
         </LocaleSwitcherSelect>
     );
 }
