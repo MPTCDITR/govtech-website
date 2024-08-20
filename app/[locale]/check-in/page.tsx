6318b9a4d67e22c9903741e9816ea456815af298
Raksmey <rsmey8373@gmail.com>
2024-08-20 14:24:50 +0700

    fix: remove useEffect and useState

--- app/[locale]/check-in/page.tsx
+++ app/[locale]/check-in/page.tsx
@@ -1,15 +1,24 @@
+import { fetchCheckInHistory } from '@/app/api/check-in-history/route';
 import HistoryCheckInCard from '@/components/history-check-in/history-check-in-card';
-import { Button } from '@/components/ui/button';
-import CheckInHistory from './checkInHistory/page';
+import { validateRequest } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
-export default function CheckIn() {
+export default async function CheckIn() {
+    const { user } = await validateRequest();
+
+    if (!user) {
+        redirect('/');
+    }
+
+    const checkInHistory = await fetchCheckInHistory(user.id);
     return (
         <main className='container mx-auto px-4 py-10 lg:px-auto'>
             <div className='flex flex-col gap-6 lg:px-20'>
-                <div className='flex flex-col items-center gap-4'>
-                    <Button className='w-3/4 text-white'>Check In</Button>
-                </div>
-                <CheckInHistory />
+                <ul className='flex flex-col gap-4'>
+                    {checkInHistory.map((CheckInHistory, index) => (
+                        <HistoryCheckInCard key={index} checkIn={CheckInHistory} />
+                    ))}
+                </ul>
             </div>
         </main>
     );
