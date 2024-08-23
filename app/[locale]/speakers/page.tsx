ece10e7a2643e9c25ec61361e2440174ec57f2d8
Raksmey <rsmey8373@gmail.com>
2024-08-23 17:25:24 +0700

    fix: speaker card title

--- app/[locale]/speakers/page.tsx
+++ app/[locale]/speakers/page.tsx
@@ -3,16 +3,16 @@ import SpeakerCardList from '@/components/speaker-card/speaker-card-list';
 
 export default function SpeakerPage() {
     return (
-        <main className='mt-4 mb-20 flex justify-center'>
-            <div className='w-full max-w-7xl px-4'>
-                <div className='text-center'>
-                    <TextElement variant='heading' className='mt-4 mb-10 text-primary'>
-                        Our Speakers
-                    </TextElement>
-                    <div>
-                        <SpeakerCardList />
-                    </div>
-                </div>
+        <main className='container flex flex-col items-center justify-between gap-8 p-4'>
+            <TextElement variant='heading' className='mt-4 text-primary'>
+                Our Speakers
+            </TextElement>
+            <TextElement variant='body'>
+                The GovTech Conference will convene on September 17th, 2024 at CADT, Phnom Penh,
+                Kingdom of Cambodia.
+            </TextElement>
+            <div>
+                <SpeakerCardList />
             </div>
         </main>
     );
