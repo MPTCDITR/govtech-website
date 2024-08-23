1bf7d4afdb8961ee2d0b1f836daf9fb96dec018a
PonlokTTR <ponlokpov2@gmail.com>
2024-08-23 10:08:18 +0700

    feat: improve speaker card change style

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -34,6 +34,10 @@ export default function Home() {
                 >
                     Our Speakers
                 </TextElement>
+                <TextElement variant='body' className='mt-4 mb-10 text-center '>
+                    The GovTech Conference will convene on September 17th, 2024 at CADT, Phnom
+                    Penh, Kingdom of Cambodia.
+                </TextElement>
                 <SpeakerCardList />
                 <div className='mt-3 flex justify-center'>
                     <Link href='/speakers'>
--- app/[locale]/speakers/page.tsx
+++ app/[locale]/speakers/page.tsx
@@ -12,6 +12,10 @@ export default function SpeakerPage() {
                     >
                         Our Speakers
                     </TextElement>
+                    <TextElement variant='body' className='mt-4 mb-10 text-center '>
+                        The GovTech Conference will convene on September 17th, 2024 at CADT,
+                        Phnom Penh, Kingdom of Cambodia.
+                    </TextElement>
                     <div>
                         <SpeakerCardList />
                     </div>
