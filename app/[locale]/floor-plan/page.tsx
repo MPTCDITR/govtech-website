ce5d7ff331e1702c97a6fcef1e63db7dd317871b
notFound217 <rsmey8373@gmail.com>
2024-08-28 10:47:15 +0700

    fix: refactor container

--- app/[locale]/floor-plan/page.tsx
+++ app/[locale]/floor-plan/page.tsx
@@ -2,20 +2,25 @@ import TextElement from '@/components/TextElement';
 
 export default function FloorPlan() {
     return (
-        <main className='container flex flex-col items-center justify-between gap-8 p-4'>
-            <TextElement variant='heading' className='mt-4 text-primary'>
-                Floor Plan
-            </TextElement>
-            <TextElement variant='body'>
-                The GovTech Conference will convene on September 17th, 2024 at CADT, Phnom Penh,
-                Kingdom of Cambodia.
-            </TextElement>
-            <img
-                key='govtech-2024-floor-plan'
-                src='/dgf-floor-plan.jpg'
-                alt='govtech-2024-floor-plan'
-                className='m-4'
-            />
+        <main className='container flex flex-col items-center justify-between gap-8'>
+            <div className='w-full'>
+                <TextElement
+                    variant='heading'
+                    className='container mx-auto py-4 text-center text-primary'
+                >
+                    Floor Plan
+                </TextElement>
+                <TextElement variant='body' className='container mx-auto px-4 py-4 text-center'>
+                    The GovTech Conference will convene on September 17th, 2024 at CADT, Phnom
+                    Penh, Kingdom of Cambodia.
+                </TextElement>
+                <img
+                    key='govtech-2024-floor-plan'
+                    src='/dgf-floor-plan.jpg'
+                    alt='govtech-2024-floor-plan'
+                    className='m-4'
+                />
+            </div>
         </main>
     );
 }
