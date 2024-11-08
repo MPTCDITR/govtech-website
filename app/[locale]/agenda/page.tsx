cc64730bbf010ab7c706b39e24b8ad18f1434193
Boti <s.visethboti@gmail.com>
2024-11-09 01:10:08 +0700

    feat: linting

--- app/[locale]/agenda/page.tsx
+++ app/[locale]/agenda/page.tsx
@@ -225,17 +225,14 @@ export default function Agenda() {
     const t = useTranslations('Agenda');
     return (
         <main className='flex flex-col items-center justify-center'>
-            <div className='w-full py-8 px-4 sm:w-3/4 sm:px-0 md:w-2/3'>
+            <div className='w-full px-4 py-8 sm:w-3/4 sm:px-0 md:w-2/3'>
                 <Text
                     variant='heading'
                     className='container mx-auto py-4 text-center text-primary'
                 >
                     {t('agenda')}
                 </Text>
-                <Text
-                    variant='subheading'
-                    className='container mx-auto px-4 py-2 text-center'
-                >
+                <Text variant='subheading' className='container mx-auto px-4 py-2 text-center'>
                     Towards a Smart Government
                 </Text>
                 <Text variant='body' className='container mx-auto px-4 py-2 text-center italic'>
